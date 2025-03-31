37195ac360a9449d73c5c684f61337339124903b
absolute162 <absolutehelix999@gmail.com>
2025-03-31 10:42:27 +0700

    Add files via upload

---
+++ js/gamenews.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#search").keyup(function() {
+        let searchText = $(this).val();
+        if (searchText != "") {
+            $.ajax({
+                url: "acgamenews.php",
+                method: "post",
+                data: {
+                    query: searchText
+                },
+                success: function(response) {
+                    $("#show-list").html(response);
+                }
+            })
+        } else {
+            $("#show-list").html("");
+        }
+    })
+
+    $(document).on('click', 'a', function() {
+        $("#search").val($(this).text())
+        $("#show-list").html("");
+    })
+})
